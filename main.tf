316ab48fd041168336e6deae35872010e7128238
Raghu K <raghuk.vit@gmail.com>
2022-06-16 07:57:00 +0530

    Update roboshop-infra/shipping-infra/

--- main.tf
+++ main.tf
@@ -8,18 +8,19 @@ module "vpc" {
 }
 
 module "mysql" {
-  depends_on          = [module.vpc]
-  source              = "git::https://github.com/raghudevopsb64/tf-module-rds-mysql.git"
-  ENGINE              = var.ENGINE
-  ENGINE_VERSION      = var.ENGINE_VERSION
-  INSTANCE_CLASS      = var.INSTANCE_CLASS
-  NAME                = var.NAME
-  SKIP_FINAL_SNAPSHOT = var.SKIP_FINAL_SNAPSHOT
-  ENV                 = var.ENV
-  COMPONENT           = var.COMPONENT
-  VPC_ID              = module.vpc.VPC_ID
-  VPC_CIDR            = module.vpc.VPC_CIDR
-  SUBNET_IDS          = module.vpc.SUBNET_IDS
+  depends_on             = [module.vpc]
+  source                 = "git::https://github.com/raghudevopsb64/tf-module-rds-mysql.git"
+  ENGINE                 = var.ENGINE
+  ENGINE_VERSION         = var.ENGINE_VERSION
+  INSTANCE_CLASS         = var.INSTANCE_CLASS
+  NAME                   = var.NAME
+  SKIP_FINAL_SNAPSHOT    = var.SKIP_FINAL_SNAPSHOT
+  ENV                    = var.ENV
+  COMPONENT              = var.COMPONENT
+  VPC_ID                 = module.vpc.VPC_ID
+  VPC_CIDR               = module.vpc.VPC_CIDR
+  SUBNET_IDS             = module.vpc.SUBNET_IDS
+  PRIVATE_HOSTED_ZONE_ID = module.vpc.PRIVATE_HOSTED_ZONE_ID
 }
 
 module "app" {
