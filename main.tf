c13bcab99f19b6b0497d33ce089763ddb6a341e4
Raghu K <raghuk.vit@gmail.com>
2022-06-18 08:04:22 +0530

    Update roboshop-infra/shipping-infra/

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "app" {
   NODE_TYPE               = var.EC2_NODE_TYPE
   PORT                    = var.PORT
   WORKSTATION_IP          = var.WORKSTATION_IP
-  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR]
+  VPC_ACCESS_TO_ALB       = [module.vpc.VPC_CIDR, "10.10.5.0/24"]
   IAM_POLICY_CREATE       = true
   PRIVATE_HOSTED_ZONE_ID  = module.vpc.PRIVATE_HOSTED_ZONE_ID
 }
